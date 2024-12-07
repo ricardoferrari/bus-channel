4fe9db4e0998e58b13e6717b046732b5a67fc08f
Ricardo A. Ferrari <ricardoaugusto.ferrari@gmail.com>
2024-12-07 14:52:24 -0300

    feat(crypto): add crypto messages

--- projects/enigma/src/app/app.component.ts
+++ projects/enigma/src/app/app.component.ts
@@ -11,15 +11,61 @@ export class AppComponent {
   title = 'enigma';
 
   public message = '';
-  public key = '';
+  public crypted_message = '';
+  public key: CryptoKey | undefined;
 
   constructor(private readonly broadcastChannelService: BroadcastChannelService) {}
 
   sendMessage(_message: string) {
-    this.broadcastChannelService.sendMessage(_message, 'crypto');
+    this.cryptMessage(_message).then((crypted_message) => {
+      const dec = new TextDecoder();
+      this.crypted_message = dec.decode(crypted_message.cyphertext);
+      this.broadcastChannelService.sendMessage(crypted_message, 'crypto');
+    });
   }
 
-  sendKey(_message: string) {
-    this.broadcastChannelService.sendMessage(_message, 'public-key');
+  sendKey() {
+    this.generateKey().then(async (key) => {
+      this.key = key;
+      const exportedKey:ArrayBuffer = await window.crypto.subtle.exportKey("raw", key);
+      this.broadcastChannelService.sendMessage(exportedKey, 'public-key');
+    });
+  }
+
+  private async generateKey(): Promise<CryptoKey> {
+    return await crypto.subtle.generateKey(
+      {
+        name: 'AES-GCM',
+        length: 256
+      },
+      true,
+      ['encrypt', 'decrypt']
+    );
+  }
+
+  private async cryptMessage(message: string): Promise<{cyphertext: ArrayBuffer, iv:Uint8Array}> {
+    if (!this.key) {
+      return {cyphertext: new ArrayBuffer(0), iv: new Uint8Array(0)};
+    }
+
+    // create a random 96-bit initialization vector (IV)
+    const iv = crypto.getRandomValues(new Uint8Array(12));
+
+    // encode the message as an ArrayBuffer
+    const enc = new TextEncoder();
+    const encoded = enc.encode(message);
+
+    // encrypt the message
+    return {
+      cyphertext: await crypto.subtle.encrypt(
+        {
+          name: 'AES-GCM',
+          iv: iv
+        },
+        this.key,
+        encoded
+      ),
+      iv: iv
+    }
   }
 }
--- projects/turing-display/src/app/app.component.ts
+++ projects/turing-display/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, signal, WritableSignal, ChangeDetectionStrategy, AfterViewChecked } from '@angular/core';
+import { Component, OnDestroy, signal, WritableSignal, ChangeDetectionStrategy, AfterViewChecked, Signal, computed } from '@angular/core';
 import { BroadcastChannelService } from 'broadcast-channel';
 import { Subscription } from 'rxjs';
 
@@ -13,20 +13,46 @@ export class AppComponent implements OnDestroy, AfterViewChecked {
 
   private subscription: Subscription = new Subscription();
 
-  receivedMessage: WritableSignal<string> = signal('');
-  receivedKey: WritableSignal<string> = signal('');
+  receivedMessage: WritableSignal<{cyphertext: ArrayBuffer, iv:Uint8Array}> = signal({cyphertext: new ArrayBuffer(0), iv: new Uint8Array(0)});
+  cryptedMessage: Signal<string> = computed(() => {
+    const dec = new TextDecoder();
+    return dec.decode(this.receivedMessage().cyphertext);
+  });
+  decryptedMessage: Signal<Promise<string>> = computed( async () => {
+    if (this.receivedKey() && this.receivedMessage().cyphertext.byteLength > 0) {
+      const cryptoKey = this.receivedKey()!;
+      return this.decryptMessage(cryptoKey, this.receivedMessage().iv, this.receivedMessage().cyphertext)
+        .then((decrypted) => decrypted)
+        .catch((error) => {
+          console.error('Error decrypting message:', error);
+          return 'Not possible to decrypt message with the provided key.';
+        });
+    } else {
+      return '';
+    }
+  });
+  receivedKey: WritableSignal<CryptoKey | undefined> = signal(undefined);
+  rawKey: WritableSignal<string> = signal('');
 
   constructor(
     private readonly broadcastChannelService: BroadcastChannelService,
   ) {
     this.subscription.add(
-      this.broadcastChannelService.messagesObservable('crypto').subscribe((message: string) => {
+      // NOTE: Subscribes for messages on the 'crypto' channel
+      this.broadcastChannelService.messagesObservable('crypto').subscribe((message: any) => {
         this.receivedMessage.set(message);
       })
     );
+
+    // NOTE: Subscribes for keys on the 'public-key' channel
     this.subscription.add(
-      this.broadcastChannelService.messagesObservable('public-key').subscribe((key: string) => {
-        this.receivedKey.set(key);
+      this.broadcastChannelService.messagesObservable('public-key').subscribe(async (rawKey: any) => {
+        this.rawKey.set(new TextDecoder().decode(rawKey));
+        const importedKey = await window.crypto.subtle.importKey("raw", rawKey, "AES-GCM", true, [
+          "encrypt",
+          "decrypt",
+        ]);
+        this.receivedKey.set(importedKey);
       })
     );
   }
@@ -37,5 +63,19 @@ export class AppComponent implements OnDestroy, AfterViewChecked {
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  private async decryptMessage(key: CryptoKey, iv: Uint8Array, ciphertext: ArrayBuffer): Promise<string> {
+    let decrypted = await window.crypto.subtle.decrypt(
+      {
+        name: "AES-GCM",
+        iv: iv
+      },
+      key,
+      ciphertext
+    );
+
+    const dec = new TextDecoder();
+    return dec.decode(decrypted);
   }
 }
